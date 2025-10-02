7e23f00765502d342bd157ac8f827ce4bb009a1d
Boris Ushakov <inlvert@gmail.com>
2025-10-02 13:55:15 +0300

    add default export

--- src/serverless.ts
+++ src/serverless.ts
@@ -1,26 +1,22 @@
-// lambda.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverless from 'serverless-http';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
+import serverless from 'serverless-http';
 
 const expressApp = express();
-
 let server: any;
 
 async function bootstrap() {
   if (!server) {
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), {
-      logger: ['error', 'warn', 'log'],
-    });
-    await app.init(); // ініціалізуємо Nest у Express
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    await app.init();
     server = serverless(expressApp);
   }
   return server;
 }
 
-export const handler = async (event: any, context: any) => {
-  const server = await bootstrap();
-  return server(event, context);
-};
+export default async function (req: any, res: any) {
+  const srv = await bootstrap();
+  return srv(req, res);
+}
